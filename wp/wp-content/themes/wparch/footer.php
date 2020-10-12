bcfc499c45cc8cf1249241d0b9fb5483051eed48
Milad Abooali <m.abooali@hotmail.com>
2020-10-12 09:44:46 +0330

    Footer E

--- wp/wp-content/themes/wparch/footer.php
+++ wp/wp-content/themes/wparch/footer.php
@@ -1,66 +1,61 @@
 
-<!-- start footer -->
+<!-- Footer -->
 <div class="container-fluid bg-gray">
-
-
-<div class="container">
-<div class="row">
-<div class="col-md-12">
-<div class="row">
-<div class="col-md-4"> <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
-	<a href="https://www.arch.ir/fa/News-View-4973.html" title="مهندسین مشاور معماری">مهندسین مشاور معماری</a><br />
-	<a href="http://decoboom.ir/%D8%AF%DA%A9%D9%88%D8%B1%DB%8C-%D9%87%D8%A7%DB%8C-%D8%B3%D8%A7%D8%AF%D9%87-%D8%A8%D8%A7-%D9%88%D8%B3%D8%A7%DB%8C%D9%84-%D8%AF%D9%88%D8%B1-%D8%B1%DB%8C%D8%AE%D8%AA%D9%86%DB%8C/" title="کار دستی با وسایل">کاردستی با وسایل دور ریختنی</a><br />
-	<a href="https://www.arch.ir/fa/News-Category-296.html" title="طراحی معماری اداری">طراحی دفتر کار اداری</a></section>
-<section style=" margin-top: 32px;">
-	<h2>
-		عضویت در گروه معماران آرل</h2>
-	<form action="https://feedburner.google.com/fb/a/mailverify" method="post" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=arch/NOpl', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" style="margin-top: 50px;   margin-bottom: 26px;" target="popupwindow">
-		<input name="email" type="text" />
-		<div style="text-align: center;">
-			<input name="uri" type="hidden" value="arch/NOpl" /><input name="loc" type="hidden" value="en_US" /> <input class="btn btn-warning" style="    width: 60%;" type="submit" value="عضــویــــت" /></div>
-		<p style="text-align: center;">
-			بعد از ثبت ایمیل ، حتما از طریق لینک فعال سازی ، عضویت خود را فعال کنید.</p>
-	</form>
-</section>
-<br />
- </div>
-<div class="col-md-4"> <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
-	<a href="https://www.arch.ir/fa/News-View-2933.html">طراحی داخلی کلینیک دندانپزشکی</a><br />
-	<a href="https://www.arch.ir/fa/News-Category-293.html">طراحی و دکوراسیون داخلی</a><br />
-	<a href="https://www.arch.ir/fa/News-View-8192.html" title="طراحی و دکوراسیون داخلی کلاسیک و لاکچری">طراحی دفتر کار کلاسیک</a></section>
-<section class="row col-md-offset-3">
-	<div class="col-md-12  " style="margin-top: 32px;">
-		<svg preserveaspectratio="xMidYMid meet" version="1.0" viewbox="0 0 4000 2420" width="100%" xmlns="http://www.w3.org/2000/svg"> <g fill="#000000" id="layer101" stroke="none"> <path d="M3175 2294 c-11 -9 -15 -18 -10 -20 28 -12 38 -25 45 -63 6 -31 4 -41 -6 -41 -27 0 -110 -33 -141 -56 -36 -28 -73 -83 -83 -128 -7 -25 -8 -20 -9 30 -2 123 -93 187 -196 138 -57 -27 -65 -58 -65 -264 0 -193 6 -222 52 -254 34 -24 129 -21 161 4 35 28 51 81 44 154 -4 58 2 51 17 -17 4 -16 25 -48 47 -72 54 -58 124 -85 224 -85 l75 0 0 295 c0 324 -2 333 -59 374 -34 25 -68 26 -96 5z m-315 -404 c0 -153 -1 -160 -20 -160 -19 0 -20 7 -20 160 0 153 1 160 20 160 19 0 20 -7 20 -160z m370 0 c0 -155 -1 -160 -20 -160 -31 0 -74 25 -103 59 -31 37 -42 96 -27 149 13 51 80 112 122 112 l28 0 0 -160z"></path> <path d="M3317 2296 c-17 -13 -17 -15 7 -40 l26 -25 0 -306 0 -305 55 0 55 0 0 220 0 220 28 -6 c88 -21 135 -76 135 -158 0 -75 -46 -148 -98 -158 -17 -3 -20 -12 -23 -62 l-3 -58 33 7 c84 18 149 74 192 164 35 74 34 127 -4 207 -42 88 -125 151 -221 169 -32 6 -37 11 -47 51 -21 77 -87 116 -135 80z"></path> <path d="M600 2286 c0 -3 9 -15 20 -26 19 -19 20 -33 20 -330 l0 -310 50 0 50 0 0 315 0 315 -25 16 c-25 16 -115 32 -115 20z"></path> <path d="M1390 2282 c0 -4 9 -13 20 -20 19 -12 20 -23 20 -327 l0 -315 50 0 50 0 0 314 0 315 -26 20 c-26 21 -114 31 -114 13z"></path> <path d="M335 2274 c-52 -13 -145 -60 -145 -74 0 -3 48 -5 108 -5 123 0 170 -17 210 -75 21 -32 22 -39 22 -301 l0 -269 45 0 45 0 0 288 c0 273 -1 289 -21 321 -26 43 -54 67 -104 91 -42 20 -126 32 -160 24z"></path> <path d="M958 2270 c-33 -10 -98 -49 -98 -59 0 -3 32 -5 70 -3 84 5 153 -11 198 -44 60 -46 62 -59 62 -319 l0 -235 45 0 45 0 0 262 c0 251 -1 263 -21 292 -62 89 -198 137 -301 106z"></path> <path d="M1317 2163 c-4 -3 -7 -143 -7 -310 l0 -303 50 0 50 0 0 310 0 310 -43 0 c-24 0 -47 -3 -50 -7z"></path> <path d="M1609 2141 l-29 -29 0 -281 0 -281 45 0 45 0 0 260 0 260 25 0 25 0 0 -185 c0 -156 3 -190 16 -211 24 -35 65 -54 120 -54 39 0 51 5 80 34 l34 34 0 186 0 186 46 0 45 0 -23 -32 c-22 -31 -23 -42 -23 -191 l0 -159 33 -29 c32 -29 34 -29 148 -29 180 0 198 20 192 227 -3 109 -6 134 -25 171 l-23 42 45 0 45 0 0 -186 0 -186 34 -34 c31 -31 40 -34 90 -34 47 0 61 4 88 29 l33 29 3 203 c3 222 -2 250 -49 274 -40 21 -109 20 -109 -2 0 -15 -1 -15 -18 0 -15 14 -42 17 -158 17 -88 0 -144 -4 -151 -11 -8 -8 -13 -8 -17 0 -10 15 -238 15 -253 0 -8 -8 -13 -8 -17 0 -5 7 -54 11 -137 11 l-131 0 -29 -29z m251 -251 c0 -153 -1 -160 -20 -160 -19 0 -20 7 -20 160 0 153 1 160 20 160 19 0 20 -7 20 -160z m355 154 c9 -4 25 -24 35 -45 16 -31 20 -59 20 -149 l0 -110 -75 0 -75 0 0 94 c0 120 6 150 40 186 31 33 31 33 55 24z m365 -154 c0 -153 -1 -160 -20 -160 -19 0 -20 7 -20 160 0 153 1 160 20 160 19 0 20 -7 20 -160z"></path> <path d="M770 1855 l0 -305 45 0 45 0 0 305 0 305 -45 0 -45 0 0 -305z"></path> <path d="M997 2062 c-20 -22 -22 -63 -5 -80 30 -30 98 -1 98 41 0 23 -35 57 -58 57 -11 0 -26 -8 -35 -18z"></path> <path d="M640 1526 c0 -58 3 -67 26 -85 25 -20 39 -21 245 -21 l219 0 0 50 0 50 -190 0 -190 0 0 35 0 35 -55 0 -55 0 0 -64z"></path> <path d="M3360 1545 l0 -45 155 0 155 0 0 45 0 45 -155 0 -155 0 0 -45z"></path> <path d="M3360 1450 l0 -30 155 0 155 0 0 30 0 30 -155 0 -155 0 0 -30z"></path> <path d="M1811 1314 c-30 -8 -82 -28 -115 -45 -60 -30 -161 -104 -169 -124 -3 -6 30 -10 87 -10 132 0 212 -40 257 -127 17 -34 19 -61 19 -310 l0 -273 -33 -1 -32 0 0 265 c0 299 -1 305 -73 355 -52 35 -120 56 -200 59 -57 2 -65 0 -83 -23 -33 -42 -77 -141 -99 -225 -65 -241 57 -527 284 -664 211 -127 460 -118 666 23 59 40 130 106 130 120 0 8 -56 10 -197 8 -109 -2 -209 0 -221 3 -35 9 -72 53 -72 86 0 25 4 29 30 29 18 0 39 -10 54 -25 l25 -25 219 0 219 0 26 57 c37 84 50 137 54 236 4 76 1 100 -26 185 -37 119 -85 196 -177 283 -75 70 -163 119 -260 144 -74 19 -241 19 -313 -1z m193 -245 l26 -20 0 -270 0 -269 -40 0 -40 0 0 268 c0 226 -2 271 -16 290 -15 21 -14 22 14 22 16 0 41 -9 56 -21z m128 -65 c8 -19 13 -113 16 -276 l4 -248 -36 0 -36 0 0 301 0 302 20 -25 c11 -14 25 -39 32 -54z"></path> </g> <g fill="#faa61a" id="layer102" stroke="none"> <path d="M1811 1314 c-30 -8 -82 -28 -115 -45 -60 -30 -161 -104 -169 -124 -3 -6 30 -10 87 -10 132 0 212 -40 257 -127 17 -34 19 -61 19 -310 l0 -273 -33 -1 -32 0 0 265 c0 299 -1 305 -73 355 -52 35 -120 56 -200 59 -57 2 -65 0 -83 -23 -33 -42 -77 -141 -99 -225 -65 -241 57 -527 284 -664 211 -127 460 -118 666 23 59 40 130 106 130 120 0 8 -56 10 -197 8 -109 -2 -209 0 -221 3 -35 9 -72 53 -72 86 0 25 4 29 30 29 18 0 39 -10 54 -25 l25 -25 219 0 219 0 26 57 c37 84 50 137 54 236 4 76 1 100 -26 185 -37 119 -85 196 -177 283 -75 70 -163 119 -260 144 -74 19 -241 19 -313 -1z m193 -245 l26 -20 0 -270 0 -269 -40 0 -40 0 0 268 c0 226 -2 271 -16 290 -15 21 -14 22 14 22 16 0 41 -9 56 -21z m128 -65 c8 -19 13 -113 16 -276 l4 -248 -36 0 -36 0 0 301 0 302 20 -25 c11 -14 25 -39 32 -54z"></path> </g> </svg></div>
-	<div class="col-md-12">
-		<div class="col-xs-4">
-			<a href="https://instagram.com/arch.ir"> <img alt="" class="img-responsive center-block" src="/uploads/myimages/icon/Footer_Icon/Instagram.png" /></a></div>
-		<div class="col-xs-4">
-			<a href="https://telegram.me/archarch"><img alt="" class="img-responsive center-block" src="/uploads/myimages/icon/Footer_Icon/telegram.png" /></a></div>
-		<div class="col-xs-4">
-			<a href="mail:info@arch.ir"> <img alt="" class="img-responsive center-block" src="/uploads/myimages/icon/Footer_Icon/email.png" /></a></div>
-	</div>
-</section>
-<br />
- </div>
-<div class="col-md-4"> <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
-	<a href="https://www.arch.ir/fa/News-Category-628.html">معماری و طراحی هتل</a><br />
-	<a href="https://www.arch.ir/fa/News-Category-296.html">طراحی و اجرا ساختمان اداری</a><br />
-	<a href="https://www.arch.ir/fa/News-Category-626.html" title="طراحی و دکوراسیون داروخانه">طراحی داخلی و معماری داروخانه</a></section>
-<section id="about_footer" style="    direction: rtl; margin-top: 32px;">
-	&nbsp;</section>
-<link href="https://www.arch.ir/" itemprop="url" />
-<strong><span itemprop="name">تماس با گروه معماران آرل</span></strong>
-<div itemprop="address" itemscope="" itemtype="https://schema.org/PostalAddress">
-	<span itemprop="streetAddress"><a href="https://www.google.com/maps/@35.8121233,51.4494243,21z" itemprop="url" target="_blank" title="آدرس شرکت آرل"><span style="
-    color: #fa9b1d;
-"><strong>ما اینجا هستیم</strong></span></a> : تهران ،&nbsp;<span itemprop="streetAddress">نیاوران، سه راه یاسر،ساختمان آوا 2،<br />
-	پلاک 250، طبقه 3، واحد 34</span></span></div>
-<strong>تلفن&nbsp; :&nbsp; </strong>۶-۲۲۹۰۱۹۰۵-۰۲۱&nbsp;<br />
-<strong>فکس</strong> :&nbsp; &nbsp; ۲۲۹۰۱۹۰۷-۰۲۱<br />
-<strong>ایمیل</strong>: info@arch.ir<br />
- </div>
-</div> </div> </div> </div></div>
-
-
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-4">
+            <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
+              <?php dynamic_sidebar( 'sidebar-4' ); ?>
+            </section>
+            <section style=" margin-top: 32px;">
+            	<h2>
+            		عضویت در گروه معماران آرل</h2>
+            	<form action="https://feedburner.google.com/fb/a/mailverify" method="post" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=arch/NOpl', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" style="margin-top: 50px;   margin-bottom: 26px;" target="popupwindow">
+            		<input name="email" type="text" />
+            		<div style="text-align: center;">
+            			<input name="uri" type="hidden" value="arch/NOpl" /><input name="loc" type="hidden" value="en_US" /> <input class="btn btn-warning" style="    width: 60%;" type="submit" value="عضــویــــت" /></div>
+            		<p style="text-align: center;">
+            			بعد از ثبت ایمیل ، حتما از طریق لینک فعال سازی ، عضویت خود را فعال کنید.</p>
+            	</form>
+            </section>
+          </div>
+          <div class="col-md-4">
+            <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
+              <?php dynamic_sidebar( 'sidebar-3' ); ?>
+            </section>
+            <section class="row col-md-offset-3">
+              <div class="col-md-12  " style="margin-top: 32px;">
+                LOGO
+              </div>
+              <div class="col-md-12">
+                Social
+              </div>
+            </section>
+          </div>
+          <div class="col-md-4">
+            <section class="row" style="    margin-top: 32px;    padding-right: 32px;    padding-bottom: 32px;    border-bottom: solid 1px;">
+              <?php dynamic_sidebar( 'sidebar-2' ); ?>
+            </section>
+            <section id="about_footer" style="    direction: rtl; margin-top: 32px;">
+              <link href="https://www.arch.ir/" itemprop="url" />
+              <strong><span itemprop="name">تماس با گروه معماران آرل</span></strong>
+              <div itemprop="address" itemscope="" itemtype="https://schema.org/PostalAddress">
+                <span itemprop="streetAddress"><a href="https://www.google.com/maps/@35.8121233,51.4494243,21z" itemprop="url" target="_blank" title="آدرس شرکت آرل"><span style="
+                color: #fa9b1d;
+                "><strong>ما اینجا هستیم</strong></span></a> : تهران ،&nbsp;<span itemprop="streetAddress">نیاوران، سه راه یاسر،ساختمان آوا 2،<br />
+                پلاک 250، طبقه 3، واحد 34</span></span>
+              </div>
+              <strong>تلفن&nbsp; :&nbsp; </strong>۶-۲۲۹۰۱۹۰۵-۰۲۱&nbsp;<br />
+              <strong>فکس</strong> :&nbsp; &nbsp; ۲۲۹۰۱۹۰۷-۰۲۱<br />
+              <strong>ایمیل</strong>: info@arch.ir<br />
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?php wp_footer(); ?>
 </body></html>
