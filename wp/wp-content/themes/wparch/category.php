c22c123e1ae93d65eeb71ec3aea238e80f5682b9
Milad Abooali <m.abooali@hotmail.com>
2020-10-12 05:50:20 +0330

    Cat S

--- wp/wp-content/themes/wparch/category.php
+++ wp/wp-content/themes/wparch/category.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The main template file
+ * The category template file
  *
- * This Site Index Page by Milad Abooali (Codebox.ir) - 2:52 AM 4/6/2020
+ * This category Page by Milad Abooali (Codebox.ir) - 2:52 AM 4/6/2020
  *
  * @package wpArch
  */
@@ -12,12 +12,21 @@
  **/
 define('CB_Description', (get_post_meta(get_the_ID(), '_cb_seo', true)['CB_Description']) ?? get_the_excerpt());
 
-
 get_header();
 ?>
 <div class="container-fluid">
   <div class="container starter_margin_top">
     <div class="row">
+      <div class="col-md-12" id="Posts">
+        <div class="row categorydescription">
+        	<div class="col-md-12">
+        		<h1><a href="<?= get_current_archive_link() ?>"><?php single_cat_title(); ?></a></h1>
+        	</div>
+        	<div class="col-md-12 text-justify">
+            <?= category_description(); ?>
+        	</div>
+        </div>
+      </div>
       <!-- Posts -->
       <div class="col-md-8" id="Posts">
         <?php
