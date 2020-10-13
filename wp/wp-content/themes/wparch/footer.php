c53ba9bb3ae722d094d7da30bff7cf9a06114f8b
Milad Abooali <m.abooali@hotmail.com>
2020-10-13 20:08:09 +0330

    EEE

--- wp/wp-content/themes/wparch/footer.php
+++ wp/wp-content/themes/wparch/footer.php
@@ -26,14 +26,23 @@
           </div>
         </div>
         <div class="row btop-1">
-          <hr>
           <div class="col-md-6">
             <section>
-              <div class="col-md-12  " style="margin-top: 32px;">
-                LOGO
+              <div class="col-md-12">
+                <img src="<?= CB_IMG ?>/icon/logo-en.png" alt="<?php bloginfo('name'); ?>">
               </div>
               <div class="col-md-12">
-                Social
+                <div class="cb-filter-grey">
+                  ما را دنبال کنید:
+                  <a target="_blank" href="#" title="Twitter"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=twitter.com" alt="twitter"></a>
+                  <a target="_blank" href="#" title="Facebook"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=facebook.com" alt="Facebook"></a>
+                  <a target="_blank" href="https://www.youtube.com/channel/UC4qS1MJx17BXWlAmZk8kZCw" title="Youtube"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=youtube.com" alt="youtube"></a>
+                  <a target="_blank" href="#" title="Pinterest"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=pinterest.com" alt="Pinterest"></a>
+                  <a target="_blank" href="#" title="Aparat"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=aparat.com" alt="Aparat"></a>
+                  <a target="_blank" href="//linkedin.com/in/farzaneh-farahbakhsh-9730711b8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8zomD0bkSCix8isTKxvZaQ%3D%3D" title="LinkedIn"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=linkedin.com" alt="LinkedIn"></a>
+                  <a target="_blank" href="//instagram.com/dalanarch" title="Instagram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=instagram.com" alt="Instagram"></a>
+                  <a target="_blank" href="//telegram.com/@Dalanarchpars" title="Telegram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=telegram.org" alt="Telegram"></a>
+                </div>
               </div>
             </section>
           </div>
--- wp/wp-content/themes/wparch/inc/head.php
+++ wp/wp-content/themes/wparch/inc/head.php
@@ -6,14 +6,14 @@
       <div class="col-md-5 col-sm-5 col-xs-12 pull-left text-left text-center-xs hidden-xs">
         <div class="hedaer-date">&nbspامروز:&nbsp<?= jdate(' l, j F Y') ?></div>
         <div class="arch_social cb-filter-grey">
-          <a target="_blank" href="https://twitter.com/" title="Twitter"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=twitter.com" alt="twitter"></a>
-          <a target="_blank" href="https://facebook.com/" title="Facebook"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=facebook.com" alt="Facebook"></a>
-          <a target="_blank" href="https://youtube.com/" title="Youtube"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=youtube.com" alt="youtube"></a>
-          <a target="_blank" href="https://pinterest.com/" title="Pinterest"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=pinterest.com" alt="Pinterest"></a>
-          <a target="_blank" href="https://aparat.com/" title="Aparat"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=aparat.com" alt="Aparat"></a>
-          <a target="_blank" href="https://linkedin.com/" title="LinkedIn"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=linkedin.com" alt="LinkedIn"></a>
-          <a target="_blank" href="https://instagram.com/" title="Instagram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=instagram.com" alt="Instagram"></a>
-          <a target="_blank" href="https://telegram.com/" title="Telegram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=telegram.org" alt="Telegram"></a>
+          <a target="_blank" href="#" title="Twitter"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=twitter.com" alt="twitter"></a>
+          <a target="_blank" href="#" title="Facebook"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=facebook.com" alt="Facebook"></a>
+          <a target="_blank" href="https://www.youtube.com/channel/UC4qS1MJx17BXWlAmZk8kZCw" title="Youtube"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=youtube.com" alt="youtube"></a>
+          <a target="_blank" href="#" title="Pinterest"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=pinterest.com" alt="Pinterest"></a>
+          <a target="_blank" href="#" title="Aparat"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=aparat.com" alt="Aparat"></a>
+          <a target="_blank" href="//linkedin.com/in/farzaneh-farahbakhsh-9730711b8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8zomD0bkSCix8isTKxvZaQ%3D%3D" title="LinkedIn"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=linkedin.com" alt="LinkedIn"></a>
+          <a target="_blank" href="//instagram.com/dalanarch" title="Instagram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=instagram.com" alt="Instagram"></a>
+          <a target="_blank" href="//telegram.com/@Dalanarchpars" title="Telegram"><img class="cb-ico cb-filter-grey" src="http://www.google.com/s2/favicons?domain=telegram.org" alt="Telegram"></a>
         </div>
       </div>
       <div class="col-md-2 col-sm-2 col-xs-12 phonenum">
